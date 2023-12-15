d65f38b758db14911bd4c46f1f8c6cc786d44920
shuri <{shurikikuti@icloud.com}>
2023-12-16 02:01:55 +1100

    deploy

--- nomad/settings.py
+++ nomad/settings.py
@@ -113,9 +113,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # メディアファイルの保存場所
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Default primary key field type
