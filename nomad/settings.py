df4a98fd284c401eb5edd26a09ca3a7c92df67d1
shuri <{shurikikuti@icloud.com}>
2023-12-16 02:19:09 +1100

    fix

--- nomad/settings.py
+++ nomad/settings.py
@@ -128,6 +128,12 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 try:
     from .local_settings import *
